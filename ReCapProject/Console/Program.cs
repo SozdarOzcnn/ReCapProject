2233ec3f5e6530edea210a651692af187554a45a
SozdarOzcnn <sozdarozcanan0@gmail.com>
2021-02-15 01:22:10 +0300

    Update

---
+++ ReCapProject/Console/Program.cs
@@ -0,0 +1,57 @@
+﻿
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new EfCarDal());
+
+            Console.WriteLine("*** TÜM ARAÇLAR ***");
+
+            foreach (var car in carManager.GetAll())
+            {
+                 Console.WriteLine("Markası {0} Rengi {1} Model Yılı {2} Günlük Fiyatı {3} Tanımı {4}",car.BrandId,car.ColorId,car.ModelYear,car.DailyPrice,car.Description);
+            }
+
+            Console.WriteLine("*** Hyundai(3) OLAN ARAÇLAR ***");
+
+
+            foreach (var car in carManager.GetCarsByBrandId(1))
+            {
+                Console.WriteLine("Markası {0} Rengi {1} Model Yılı {2} Günlük Fiyatı {3} Tanımı {4}", car.BrandId, car.ColorId, car.ModelYear, car.DailyPrice, car.Description);
+            }
+
+ 
+
+            Console.WriteLine("*** Renk Kahverengi(5) OLAN ARAÇLAR ***");
+
+
+            foreach (var car in carManager.GetCarsByColorId(5))
+            {
+                Console.WriteLine("Seçitğiniz araç {0} model yılı olup, {1} fiyatı {2}", car.ModelYear, car.Description, car.DailyPrice);
+            }
+
+            CarManager carManager1 = new CarManager(new EfCarDal());
+            carManager1.Add( 
+                new Car { BrandId=1,ColorId=6,Id=6,DailyPrice=750,Description="Renault",ModelYear=2021});
+
+
+
+            foreach (var car in carManager1.GetAll())
+            {
+                Console.WriteLine("Markası {0} Rengi {1} Model Yılı {2} Günlük Fiyatı {3} Tanımı {4}", car.BrandId, car.ColorId, car.ModelYear, car.DailyPrice, car.Description);
+            }
+
+            carManager1.Add(
+                new Car { Id=1,BrandId = 1, ColorId = 1, DailyPrice = 0, Description = "Toyota", ModelYear = 2021 });
+        }
+    }
+}
---
+++ ReCapProject/DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,69 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car> { 
+            
+                new Car{Id=1,BrandId=1,ColorId=1,ModelYear=2020,DailyPrice=550,Description="Sedan, dizel 1.5 motor"},
+                new Car{Id=2,BrandId=1,ColorId=2,ModelYear=2021,DailyPrice=650,Description="HatchBack, dizel 1.6 motor" },
+                new Car{Id=3,BrandId=2,ColorId=4,ModelYear=2019,DailyPrice=400,Description="Station, benzinli 1.5 motor" },
+                new Car{Id=4,BrandId=3,ColorId=3,ModelYear=2020,DailyPrice=750,Description=" SUV, elektrikli" }
+            };
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c=>c.Id==car.Id);
+
+            _cars.Remove(carToDelete);
+
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Car> GetById(int id)
+        {
+           return _cars.Where(c=>c.Id==id).ToList();
+        }
+
+        public Car Get(Expression<Func<Car, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+
+            carToUpdate.Description = car.Description;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            
+        }
+    }
+}
+ 
